2f4a0457ec6c586de7cddb20f2efb225356dc2ec
Mathias Claassen <mathias.c@galliot.us>
2020-04-09 16:59:59 -0300

    Always run build of test image

--- inference-test-tool/send-inference-request.sh
+++ inference-test-tool/send-inference-request.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-if [[ "$(docker images -q test_arterys_sdk 2> /dev/null)" == "" ]]; then
-    docker image build --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER}) -t test_arterys_sdk . 
-fi
-
+docker image build --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER}) -t test_arterys_sdk .
 docker run --network host --rm -v $(pwd):/opt -it test_arterys_sdk $@
