aea19f580673ae14b4d3b69f7e616f253d05380e
Mathias Claassen <mathias.c@galliot.us>
2020-06-10 12:37:17 -0300

    Remove -it flag

--- inference-test-tool/send-inference-request.sh
+++ inference-test-tool/send-inference-request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker image build --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER}) -t test_arterys_sdk .
-docker run --network host --rm -v $(pwd):/opt -it test_arterys_sdk $@
+docker run --network host --rm -v $(pwd):/opt test_arterys_sdk $@
