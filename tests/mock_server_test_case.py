ddb6dbbb0f19094e3f0aee2d1d3631a683fd3d91
Mathias Claassen <mathias.c@galliot.us>
2020-06-10 12:45:49 -0300

    Fix autogenerated message

--- tests/mock_server_test_case.py
+++ tests/mock_server_test_case.py
@@ -36,7 +36,7 @@ class MockServerTestCase(unittest.TestCase):
         self.check_service_up(8900)
 
     def check_service_up(self, port, endpoint="/", params={}):
-        for i in range(40):
+        for i in range(30):
             try:
                 response = requests.get("http://localhost:{}/healthcheck".format(port))
             except requests.exceptions.ConnectionError:
