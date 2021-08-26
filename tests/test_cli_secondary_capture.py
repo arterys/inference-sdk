b3146a60766d41388972755ee296ac7365d9fdbc
Nick Svarich <nicholas@arterys.com>
2021-08-25 18:48:10 -0600

    Reuse sc test for cli test and fix input data failure

--- tests/test_cli_secondary_capture.py
+++ tests/test_cli_secondary_capture.py
@@ -7,47 +7,10 @@ from pydicom.errors import InvalidDicomError
 from .mock_server_test_case import MockServerTestCase
 from .utils import term_colors
 
-class TestSecondaryCapture(MockServerTestCase):
-    input_dir = 'test_secondary_capture/'
-    output_dir = 'test_secondary_capture_out/'
+from tests.test_secondary_capture import TestSecondaryCapture
+
+class TestCliSecondaryCapture(TestSecondaryCapture):
+    input_dir = 'test_cli_secondary_capture/'
+    output_dir = 'test_cli_secondary_capture_out/'
     command = '--cli_model'
-    test_name = 'Secondary capture test'
-
-    def testOutputFiles(self):
-        input_files = os.listdir(os.path.join('tests/data', self.input_dir))
-        result = subprocess.run(['./send-inference-request.sh', '--segmentation_model', '--host=0.0.0.0', '--port=' +
-            self.inference_port, '--output=' + self.output_dir, '--input=' + self.input_dir] + self.additional_flags.split(),
-            cwd='inference-test-tool', stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-
-        # Test that the command executed successfully
-        self.check_success(result, command_name="Send inference request")
-        self.assertEqual(result.returncode, 0)
-
-        output_files = os.listdir(os.path.join(self.inference_test_dir, self.output_dir))
-
-        # Test JSON response
-        file_path = os.path.join(self.inference_test_dir, self.output_dir, 'response.json')
-        self.assertTrue(os.path.exists(file_path))
-
-        with open(file_path) as json_file:
-            data = json.load(json_file)
-
-        self.assertIn('protocol_version', data)
-        self.assertIn('parts', data)
-
-        # Test if the amount of binary buffers is equals to the elements in `parts`
-        output_folder_path = os.path.join(self.inference_test_dir, self.output_dir)
-        output_files = os.listdir(output_folder_path)
-        count_masks = len([f for f in output_files if f.startswith("sc_")])
-        secondary_capture_parts = [p for p in data["parts"] if p['binary_type'] == 'dicom_secondary_capture']
-        self.assertEqual(count_masks, len(secondary_capture_parts))
-
-        # Read and verify output secondary capture dicom files
-        for index, sc in enumerate(secondary_capture_parts):
-            file_path = os.path.join(output_folder_path, 'sc_' + str(index) + '.dcm')
-            try:
-                dcm = pydicom.dcmread(file_path)
-            except InvalidDicomError:
-                self.fail("output dcm file is invalid!")
-
-        print(term_colors.OKGREEN + "Secondary capture test succeeded!!", term_colors.ENDC)
+    test_name = 'CLI Secondary capture test'
