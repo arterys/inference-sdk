6e070ee2cee7413b24b7176571a4f029fc439754
Nick Svarich <nicholas@arterys.com>
2023-03-27 13:05:53 -0600

    Format

--- utils/request_handler_hf5.py
+++ utils/request_handler_hf5.py
@@ -12,7 +12,6 @@ from utils import tagged_logger
 from src import pre_processing, inference, post_processing
 
 
-
 def get_hf5_inference_response(json_input, dicom_instances):
     response_json = {
         "protocol_version": "1.0",
@@ -31,8 +30,8 @@ def get_hf5_inference_response(json_input, dicom_instances):
 
     # Run Inference
     predictions = inference.run(
-        input_images=preprocess_res["outimg"], input_meta=preprocess_res["outmeta"])
-
+        input_images=preprocess_res["outimg"], input_meta=preprocess_res["outmeta"]
+    )
 
     # Convert to h5 file bytes for the response
     predictions_h5_bytes = io.BytesIO()
@@ -47,28 +46,36 @@ def get_hf5_inference_response(json_input, dicom_instances):
 
 
 def create_h5_output(outh5, predictions, preprocess_res):
-    outh5.attrs['slices'] = len(predictions)
-    if (len(predictions) > 0):
-        outh5.attrs['height'] = predictions[0].shape[0]
-        outh5.attrs['width'] = predictions[0].shape[1]
-        outh5.attrs['timepoints'] = predictions[0].shape[2]
+    outh5.attrs["slices"] = len(predictions)
+    if len(predictions) > 0:
+        outh5.attrs["height"] = predictions[0].shape[0]
+        outh5.attrs["width"] = predictions[0].shape[1]
+        outh5.attrs["timepoints"] = predictions[0].shape[2]
     for slice_index, pred in enumerate(predictions):
-
         slice_dset = outh5.create_dataset(
-            'slice_{}_pred'.format(slice_index), data=pred, dtype=np.float32, compression="gzip"
+            "slice_{}_pred".format(slice_index),
+            data=pred,
+            dtype=np.float32,
+            compression="gzip",
         )
 
         # One value per entire dataset
-        slice_dset.attrs['version'] = 1.0
-        slice_dset.attrs['spacing'] = preprocess_res["pixel_spacing"]
-        slice_dset.attrs['rotation_angle'] = preprocess_res["rotation_angle"]
-        slice_dset.attrs['heart_rate'] = preprocess_res["heart_rate"]
-        slice_dset.attrs['image_orientation_patient'] = preprocess_res["image_orientation_patient"]
+        slice_dset.attrs["version"] = 1.0
+        slice_dset.attrs["spacing"] = preprocess_res["pixel_spacing"]
+        slice_dset.attrs["rotation_angle"] = preprocess_res["rotation_angle"]
+        slice_dset.attrs["heart_rate"] = preprocess_res["heart_rate"]
+        slice_dset.attrs["image_orientation_patient"] = preprocess_res[
+            "image_orientation_patient"
+        ]
 
         # Ordered by Arterys slice_index asc
-        slice_dset.attrs['centroid'] = preprocess_res["slice_centroids"][slice_index]
-        slice_dset.attrs['first_element_center'] = preprocess_res["first_element_centers"][slice_index]
-        slice_dset.attrs['centroid_offset'] = preprocess_res["slice_centroid_offsets"][slice_index]
+        slice_dset.attrs["centroid"] = preprocess_res["slice_centroids"][slice_index]
+        slice_dset.attrs["first_element_center"] = preprocess_res[
+            "first_element_centers"
+        ][slice_index]
+        slice_dset.attrs["centroid_offset"] = preprocess_res["slice_centroid_offsets"][
+            slice_index
+        ]
     outh5.close()
 
 
@@ -78,7 +85,5 @@ def request_handler(json_input, dicom_instances, input_digest):
     """
     transaction_logger = tagged_logger.TaggedLogger(logger)
     transaction_logger.add_tags({"input_hash": input_digest})
-    transaction_logger.info(
-        "server received json_input len={}".format(len(json_input))
-    )
+    transaction_logger.info("server received json_input len={}".format(len(json_input)))
     return get_hf5_inference_response(json_input, dicom_instances)
